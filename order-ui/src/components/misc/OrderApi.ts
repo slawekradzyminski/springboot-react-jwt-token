858634e555dde0391c1e201af91230ac8b797888
Slawek <s.radzyminski@ocado.com>
2022-12-18 11:10:50 +0100

    minor api layer refactor

--- order-ui/src/components/misc/OrderApi.ts
+++ order-ui/src/components/misc/OrderApi.ts
@@ -1,7 +1,64 @@
-import axios from 'axios'
-import { config } from '../../Constants'
 import { User, UserLocalStorage } from '../../types/user'
-import { parseJwt } from './Helpers'
+import { axiosInstance } from './axiosConfig'
+
+const json = { 'Content-type': 'application/json' }
+
+const authenticate = (username: string, password: string) => {
+  return axiosInstance.post('/auth/authenticate', { username, password }, {
+    headers: json
+  })
+}
+
+const signup = (user: User) => {
+  return axiosInstance.post('/auth/signup', user, {
+    headers: json
+  })
+}
+
+const numberOfUsers = () => axiosInstance.get('/public/numberOfUsers')
+
+const numberOfOrders = () => axiosInstance.get('/public/numberOfOrders')
+
+const getUsers = (user: UserLocalStorage, username?: string) => {
+  const url = username ? `/api/users/${username}` : '/api/users'
+  return axiosInstance.get(url, {
+    headers: { 'Authorization': bearerAuth(user) }
+  })
+}
+
+const deleteUser = (user: UserLocalStorage, username: string) => {
+  return axiosInstance.delete(`/api/users/${username}`, {
+    headers: { 'Authorization': bearerAuth(user) }
+  })
+}
+
+const getOrders = (user: UserLocalStorage, text?: string) => {
+  const url = text ? `/api/orders?text=${text}` : '/api/orders'
+  return axiosInstance.get(url, {
+    headers: { 'Authorization': bearerAuth(user) }
+  })
+}
+
+const deleteOrder = (user: UserLocalStorage, orderId: string) => {
+  return axiosInstance.delete(`/api/orders/${orderId}`, {
+    headers: { 'Authorization': bearerAuth(user) }
+  })
+}
+
+const createOrder = (user: UserLocalStorage, order: string) => {
+  return axiosInstance.post('/api/orders', order, {
+    headers: {
+      ...json,
+      'Authorization': bearerAuth(user)
+    }
+  })
+}
+
+const getUserMe = (user: UserLocalStorage) => {
+  return axiosInstance.get('/api/users/me', {
+    headers: { 'Authorization': bearerAuth(user) }
+  })
+}
 
 export const orderApi = {
   authenticate,
@@ -16,90 +73,6 @@ export const orderApi = {
   getUserMe
 }
 
-function authenticate(username: string, password: string) {
-  return instance.post('/auth/authenticate', { username, password }, {
-    headers: { 'Content-type': 'application/json' }
-  })
-}
-
-function signup(user: User) {
-  return instance.post('/auth/signup', user, {
-    headers: { 'Content-type': 'application/json' }
-  })
-}
-
-function numberOfUsers() {
-  return instance.get('/public/numberOfUsers')
-}
-
-function numberOfOrders() {
-  return instance.get('/public/numberOfOrders')
-}
-
-function getUsers(user: UserLocalStorage, username: string) {
-  const url = username ? `/api/users/${username}` : '/api/users'
-  return instance.get(url, {
-    headers: { 'Authorization': bearerAuth(user) }
-  })
-}
-
-function deleteUser(user: UserLocalStorage, username: string) {
-  return instance.delete(`/api/users/${username}`, {
-    headers: { 'Authorization': bearerAuth(user) }
-  })
-}
-
-function getOrders(user: UserLocalStorage, text: string) {
-  const url = text ? `/api/orders?text=${text}` : '/api/orders'
-  return instance.get(url, {
-    headers: { 'Authorization': bearerAuth(user) }
-  })
-}
-
-function deleteOrder(user: UserLocalStorage, orderId: string) {
-  return instance.delete(`/api/orders/${orderId}`, {
-    headers: { 'Authorization': bearerAuth(user) }
-  })
-}
-
-function createOrder(user: UserLocalStorage, order: string) {
-  return instance.post('/api/orders', order, {
-    headers: {
-      'Content-type': 'application/json',
-      'Authorization': bearerAuth(user)
-    }
-  })
-}
-
-function getUserMe(user: UserLocalStorage) {
-  return instance.get('/api/users/me', {
-    headers: { 'Authorization': bearerAuth(user) }
-  })
-}
-
-// -- Axios
-
-const instance = axios.create({
-  baseURL: config.url.API_BASE_URL
-})
-
-instance.interceptors.request.use(function (config) {
-  // If token is expired, redirect user to login
-  if (config.headers!.Authorization) {
-    // @ts-ignore
-    const token = config.headers!.Authorization.split(' ')[1]
-    const data = parseJwt(token)
-    if (Date.now() > data.exp * 1000) {
-      window.location.href = "/login"
-    }
-  }
-  return config
-}, function (error) {
-  return Promise.reject(error)
-})
-
-// -- Helper functions
-
-function bearerAuth(user: UserLocalStorage) {
+const bearerAuth = (user: UserLocalStorage) => {
   return `Bearer ${user.accessToken}`
 }
---
+++ order-ui/src/components/misc/axiosConfig.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { config } from "../../Constants";
+import { parseJwt } from "./Helpers";
+
+export const axiosInstance = axios.create({
+  baseURL: config.url.API_BASE_URL,
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    if (config.headers!.Authorization) {
+      // @ts-ignore
+      const token = config.headers!.Authorization.split(" ")[1];
+      const data = parseJwt(token);
+      if (Date.now() > data.exp * 1000) {
+        window.location.href = "/login";
+      }
+    }
+    return config
+  },
+  (error) => Promise.reject(error)
+);
