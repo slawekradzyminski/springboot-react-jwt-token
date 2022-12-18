2ad9e29957360d9d4617a3e90c63b3ad6d10be95
Slawek <s.radzyminski@ocado.com>
2022-12-18 09:02:42 +0100

    converted first component to functional

--- order-ui/src/components/home/Home.tsx
+++ order-ui/src/components/home/Home.tsx
@@ -1,73 +1,67 @@
-import React, {Component} from 'react'
-import {Container, Dimmer, Grid, Icon, Image, Loader, Segment, Statistic} from 'semantic-ui-react'
-import {orderApi} from '../misc/OrderApi'
-import {handleLogError} from '../misc/Helpers'
+import React, { useEffect, useState } from 'react'
+import { Container, Dimmer, Grid, Icon, Image, Loader, Segment, Statistic } from 'semantic-ui-react'
+import { orderApi } from '../misc/OrderApi'
+import { handleLogError } from '../misc/Helpers'
 
-class Home extends Component {
-  state = {
-    numberOfUsers: 0,
-    numberOfOrders: 0,
-    isLoading: false,
-  }
+export const Home = () => {
+  const [numberOfUsers, setNumberOfUsers] = useState<number>(0);
+  const [numberOfOrders, setNumberOfOrders] = useState<number>(0);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  async componentDidMount() {
-    this.setState({isLoading: true})
-    try {
-      let response = await orderApi.numberOfUsers()
-      const numberOfUsers = response.data
+  useEffect(() => {
+    setIsLoading(true)
+    const fetchData = async () => {
+      const usersResponse = await orderApi.numberOfUsers()
+      setNumberOfUsers(usersResponse.data)
 
-      response = await orderApi.numberOfOrders()
-      const numberOfOrders = response.data
-
-      this.setState({ numberOfUsers, numberOfOrders })
-    } catch (error) {
-      handleLogError(error)
-    } finally {
-      this.setState({ isLoading: false })
+      const ordersResponse = await orderApi.numberOfOrders()
+      setNumberOfOrders(ordersResponse.data)
     }
-  }
 
-  render() {
-    const { isLoading } = this.state
-    if (isLoading) {
-      return (
-        <Segment basic style={{ marginTop: window.innerHeight / 2 }}>
-          <Dimmer active inverted>
-            <Loader inverted size='huge'>Loading</Loader>
-          </Dimmer>
-        </Segment>
-      )
-    } else {
-      const { numberOfUsers, numberOfOrders } = this.state
-      return (
-        <Container text>
-          <Grid stackable columns={2}>
-            <Grid.Row>
-              <Grid.Column textAlign='center'>
-                <Segment color='violet'>
-                  <Statistic>
-                    <Statistic.Value><Icon name='user' color='grey' />{numberOfUsers}</Statistic.Value>
-                    <Statistic.Label>Users</Statistic.Label>
-                  </Statistic>
-                </Segment>
-              </Grid.Column>
-              <Grid.Column textAlign='center'>
-                <Segment color='violet'>
-                  <Statistic>
-                    <Statistic.Value><Icon name='laptop' color='grey' />{numberOfOrders}</Statistic.Value>
-                    <Statistic.Label>Orders</Statistic.Label>
-                  </Statistic>
-                </Segment>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
+    fetchData()
+      .catch(error => handleLogError(error))
+      .finally(() => setIsLoading(false));
+  }, [])
 
-          <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' style={{ marginTop: '2em' }} />
-          <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
-        </Container>
-      )
-    }
-  }
-}
+  const loadingFragment = () => (
+    <Segment basic style={{ marginTop: window.innerHeight / 2 }}>
+      <Dimmer active inverted>
+        <Loader inverted size='huge'>Loading</Loader>
+      </Dimmer>
+    </Segment>
+  )
+
+  const main = () => (
+    <Container text>
+      <Grid stackable columns={2}>
+        <Grid.Row>
+          <Grid.Column textAlign='center'>
+            <Segment color='violet'>
+              <Statistic>
+                <Statistic.Value><Icon name='user' color='grey' />{numberOfUsers}</Statistic.Value>
+                <Statistic.Label>Users</Statistic.Label>
+              </Statistic>
+            </Segment>
+          </Grid.Column>
+          <Grid.Column textAlign='center'>
+            <Segment color='violet'>
+              <Statistic>
+                <Statistic.Value><Icon name='laptop' color='grey' />{numberOfOrders}</Statistic.Value>
+                <Statistic.Label>Orders</Statistic.Label>
+              </Statistic>
+            </Segment>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+
+      <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' style={{ marginTop: '2em' }} />
+      <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
+    </Container>
+  )
+
+  return <>
+    {isLoading ? loadingFragment() : main()}
+  </>
+};
 
 export default Home
